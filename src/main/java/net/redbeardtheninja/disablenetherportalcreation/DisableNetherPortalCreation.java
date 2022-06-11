63be376bcb9d4771fdf5f03e4fc95d76f419b5ef
antyppdet <antypdet@gmail.com>
2022-06-10 22:48:42 -0700

    Tie it to a new gamerule

--- src/main/java/net/redbeardtheninja/disablenetherportalcreation/DisableNetherPortalCreation.java
+++ src/main/java/net/redbeardtheninja/disablenetherportalcreation/DisableNetherPortalCreation.java
@@ -1,6 +1,9 @@
 package net.redbeardtheninja.disablenetherportalcreation;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
+import net.minecraft.world.GameRules;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,7 +12,8 @@ public class DisableNetherPortalCreation implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("disablenetherportalcreation");
-
+	public static final GameRules.Key<GameRules.BooleanRule> DISABLE_NETHER_PORTAL_CREATION =
+			GameRuleRegistry.register("disableNetherPortalCreation", GameRules.Category.PLAYER, GameRuleFactory.createBooleanRule(true));
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
--- src/main/java/net/redbeardtheninja/disablenetherportalcreation/mixin/AreaHelperMixin.java
+++ src/main/java/net/redbeardtheninja/disablenetherportalcreation/mixin/AreaHelperMixin.java
@@ -1,13 +1,21 @@
 package net.redbeardtheninja.disablenetherportalcreation.mixin;
 
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.WorldAccess;
 import net.minecraft.world.dimension.AreaHelper;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import static net.redbeardtheninja.disablenetherportalcreation.DisableNetherPortalCreation.DISABLE_NETHER_PORTAL_CREATION;
+
 @Mixin(AreaHelper.class)
-public class AreaHelperMixin {
+public abstract class AreaHelperMixin {
+    @Accessor("world")
+    abstract WorldAccess getServer();
+
     @Inject(
             method = "createPortal",
             at = @At(
@@ -16,6 +24,17 @@ public class AreaHelperMixin {
             cancellable = true
     )
     private void createPortal(CallbackInfo ci) {
-        ci.cancel();
+        if (getServer() != null) {
+            MinecraftServer server = getServer().getServer();
+            if (server != null && server.getGameRules().getBoolean(DISABLE_NETHER_PORTAL_CREATION)) {
+                ci.cancel();
+            }
+            if (server == null) {
+                ci.cancel();
+            }
+        }
+        if (getServer() == null) {
+            ci.cancel();
+        }
     }
 }
